8249d64c9d365a344a0656879c102b6e5b1b964c
wolalb15 <wolalb15@htl-kaindorf.ac.at>
2018-10-11 12:18:28 +0200

    Initial commit

---
+++ src/main/java/com/example/Overcraft/OvercraftMod.java
@@ -0,0 +1,31 @@
+package com.example.Overcraft;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = OvercraftMod.MODID, name = OvercraftMod.NAME, version = OvercraftMod.VERSION)
+public class OvercraftMod
+{
+    public static final String MODID = "Overcraft";
+    public static final String NAME = "OvercraftMod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
