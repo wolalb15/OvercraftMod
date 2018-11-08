14a67d87597c30c76e12c91acaff5cf9a5a390c7
BuildTools <unconfigured@null.spigotmc.org>
2018-11-08 13:09:12 +0100

    KeyHandler with Particle

--- src/main/java/com/overcraft/KeyHandler.java
+++ src/main/java/com/overcraft/KeyHandler.java
@@ -1,9 +1,13 @@
 package com.overcraft;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.ParticleBubble;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
+
+import java.util.Random;
 
 public class KeyHandler {
 
@@ -12,8 +16,16 @@ public class KeyHandler {
         if(OvercraftMod.ult.isPressed()){
             EntityPlayer player = Minecraft.getMinecraft().player;
 
-            player.setPositionAndUpdate(player.posX + 0,player.posY+10,player.posZ + 0);
-
+            //player.setPositionAndUpdate(player.posX + 0,player.posY+10,player.posZ + 0);
+            Random rd = new Random();
+            int zz = 0;
+            double zzD;
+            for (int i = 0; i  < 150; i++){
+                zz = rd.nextInt(200) ;
+                zzD  = (double) zz / 100;
+                System.out.println(zzD);
+                player.getEntityWorld().spawnParticle(EnumParticleTypes.WATER_SPLASH,player.posX,player.posY + zzD,player.posZ,1,1,1,1);
+            }
         }
 
     }
